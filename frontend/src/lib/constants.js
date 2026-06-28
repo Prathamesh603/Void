9380a68e6679c3777ff7684b8dc842d09d89b802
Pratham Rajput <prathamrajput709@gmail.com>
2026-06-28 23:15:34 +0530

    added google cloud deployment api to the fronted for backend call

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -1,4 +1,4 @@
 export const API_BASE =
-  import.meta.env.VITE_API_URL?.replace(/\/$/, '') || 'http://127.0.0.1:8080/api'
+  import.meta.env.VITE_API_URL?.replace(/\/$/, '') || 'https://void-822012759095.europe-west1.run.app/api'
 
 export const API_ORIGIN = API_BASE.replace(/\/api$/, '');
